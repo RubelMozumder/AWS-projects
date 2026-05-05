37c90158bef2cc2e723cbb942e8e12e3e99adab1
RubelMozumder <rubel.mozumder@outlook.com>
2026-05-05 22:31:42 +0200

    Deploying stacks

--- deploy_scripts/deploy_from_s3.sh
+++ deploy_scripts/deploy_from_s3.sh
@@ -1,23 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BUCKET_NAME="bucket-for-projects-321"
 BOOTSTRAP_FOLDER="serverless-web-backend/bootstrap-stacks"
 AWS_ACCOUNT_BOOTSTRAP_FILE="account-bootstrap-stack.yaml"
-# aws s3 cp s3://bucket-for-projects-321/${BOOTSTRAP_FOLDER}/${AWS_ACCOUNT_BOOTSTRAP_FILE} .
-
 AWS_MULTI_STACK_FILE="serverless-web-backend-stack.yaml"
 SERVERLESS_WEB_FOLDER="serverless-web-backend/cloudFormation"
 
-aws s3 cp s3://bucket-for-projects-321/${SERVERLESS_WEB_FOLDER}/${AWS_MULTI_STACK_FILE} .
+# aws s3 cp "s3://${BUCKET_NAME}/${BOOTSTRAP_FOLDER}/${AWS_ACCOUNT_BOOTSTRAP_FILE}" .
+aws s3 cp "s3://${BUCKET_NAME}/${SERVERLESS_WEB_FOLDER}/${AWS_MULTI_STACK_FILE}" .
 
 # aws cloudformation deploy \
 #   --stack-name account-bootstrap \
-#   --template-file ${AWS_ACCOUNT_BOOTSTRAP_FILE} \
-#   --capabilities CAPABILITY_NAMED_IAM
+#   --template-file "${AWS_ACCOUNT_BOOTSTRAP_FILE}" \
+#   --capabilities CAPABILITY_NAMED_IAM \
+#   --no-fail-on-empty-changeset
 
 aws cloudformation deploy \
   --stack-name serverless-web-backend-stack \
-  --template-file ${AWS_MULTI_STACK_FILE} \
+  --template-file "${AWS_MULTI_STACK_FILE}" \
   --capabilities CAPABILITY_NAMED_IAM \
   --no-fail-on-empty-changeset
-
-# aws cloudformation describe-stack-events --stack-name account-bootstrap
 
 
